b1f8eea13f5f9b73eb4db1b772c6e018c822ed87
Moritz <moritz.zielke@gmail.com>
2023-07-26 12:24:31 +0200

    Format gas usage values in a table

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+use comfy_table::Table;
+use rust_decimal::Decimal;
 use workspaces::types::Gas;
 use workspaces::Contract;
 
@@ -25,6 +27,7 @@ async fn main() -> anyhow::Result<()> {
 
     let worker = workspaces::sandbox().await?;
 
+    // TODO still need `project_path_*` variables?
     let project_path_native = "./contracts/calculations";
     let wasm_calculations = workspaces::compile_project(project_path_native).await?;
     let contract_calculations = worker.dev_deploy(&wasm_calculations).await?;
@@ -33,8 +36,10 @@ async fn main() -> anyhow::Result<()> {
     let wasm_wasmi = workspaces::compile_project(project_path_wasmi).await?;
     let contract_wasmi = worker.dev_deploy(&wasm_wasmi).await?;
 
+    let mut gas_usage = GasUsage::default();
+
     for loop_limit in cli_args.loop_limit {
-        println!("loop_limit: {loop_limit}");
+        gas_usage.loop_limits.push(loop_limit);
 
         let gas_burnt_native = profile_gas_usage(
             &contract_calculations,
@@ -42,13 +47,15 @@ async fn main() -> anyhow::Result<()> {
             loop_limit,
         )
         .await?;
-        print_gas_burnt(project_path_native, gas_burnt_native);
+        gas_usage.native.push(gas_burnt_native);
 
         // Passing `wasm_calculations` to interpret it in `wasm_wasi`.
         let args: Vec<u8> = [loop_limit.to_le_bytes().to_vec(), wasm_calculations.clone()].concat();
         let gas_burnt_wasmi = profile_gas_usage(&contract_wasmi, args, loop_limit).await?;
-        print_gas_burnt(project_path_wasmi, gas_burnt_wasmi);
+        gas_usage.interpreted_wasmi.push(gas_burnt_wasmi);
     }
+
+    println!("{gas_usage}");
 
     Ok(())
 }
@@ -100,8 +107,42 @@ async fn profile_gas_usage(
     Ok(gas_burnt)
 }
 
-fn print_gas_burnt(project_path: &str, gas_burnt: Gas) {
-    println!(
-        "Gas used by the `FunctionCallAction` receipt calling\n `{METHOD_NAME}` on {project_path}:\n {gas_burnt}"
-    );
+#[derive(Default)]
+/// Collects gas usage values for different `loop_limit` values.
+struct GasUsage {
+    /// Values of `loop_limit` for which benchmarks are run.
+    loop_limits: Vec<u32>,
+    /// Gas used by executing `contracts/calculations` directly for each of the `loop_limits`.
+    native: Vec<Gas>,
+    /// Gas used by executing a contract that uses wasmi to interpret `contracts/calculations` for
+    /// each of the `loop_limits`.
+    interpreted_wasmi: Vec<Gas>,
+}
+
+/// Prints gas usage values in a table.
+impl std::fmt::Display for GasUsage {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut headers = vec!["exec_mode".to_string()];
+        let mut row_native = vec!["native".to_string()];
+        let mut row_wasmi = vec!["wasmi interpreter".to_string()];
+        for i in 0..self.loop_limits.len() {
+            headers.push(format!("loop_limit = {}", self.loop_limits[i]));
+            row_native.push(fmt_tgas(self.native[i]));
+            row_wasmi.push(fmt_tgas(self.interpreted_wasmi[i]));
+        }
+
+        let mut table = Table::new();
+        table.set_header(headers);
+        table.add_row(row_native);
+        table.add_row(row_wasmi);
+
+        write!(f, "{}", table)
+    }
+}
+
+/// Formats `gas` as teragas, rounding to four decimals.
+fn fmt_tgas(gas: Gas) -> String {
+    // Scale 12 since converting gas to tgas requires dividing by 10^12.
+    let tgas = Decimal::new(gas.try_into().unwrap(), 12);
+    format!("{} TGas", tgas.round_dp(4))
 }
