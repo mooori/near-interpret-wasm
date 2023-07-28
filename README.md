d3e50ba83cbfd2e4807b3c2000801c6da28cff05
Moritz Zielke <moritz.zielke@aurora.dev>
2023-07-28 11:18:50 +0200

    Read multiple loop limits from cli and print table (#5)
    * Read `loop_limit` from cli args
    
    * Enable multiple `loop_limit`s
    
    * Avoid deploying the same contract multiple times
    
    * Make `main` return `anyhow::Result`
    
    * Format gas usage values in a table
    
    * Code cleanup
    
    * Update/add usage, results and observations in README
    
    * Fix comment position

--- README.md
+++ README.md
@@ -1,8 +1,32 @@
+# About
+
+Execute `contracts/calculations` as Near contract and interpret it inside another Near contract by running the following command. Gas usage will be printed to standard output and the calculations are repeated `loop-limit` times.
+
 # Usage
 
 ```
-cargo run
+cargo run -- --help
 ```
+
+# Results
+
+```
+$ cargo run -- --loop-limit 1,100,1000,10000,20000
+
++-------------------+----------------+------------------+-------------------+--------------------+--------------------+
+| exec_mode         | loop_limit = 1 | loop_limit = 100 | loop_limit = 1000 | loop_limit = 10000 | loop_limit = 20000 |
++=====================================================================================================================+
+| native            | 2.6680 TGas    | 2.6729 TGas      | 2.7117 TGas       | 3.0971 TGas        | 3.5250 TGas        |
+|-------------------+----------------+------------------+-------------------+--------------------+--------------------|
+| wasmi interpreter | 73.6618 TGas   | 74.7821 TGas     | 84.8506 TGas      | 185.4982 TGas      | 297.3189 TGas      |
++-------------------+----------------+------------------+-------------------+--------------------+--------------------+
+```
+
+Observations:
+
+- Interpreter setup for `wasmi` roughly costs 70 TGas (compare gas usage for `loop_limit = 1`).
+    - This can be a deal breaker if an Ethereum XCC is to be executed in a separate interpreter instance.
+- Increasing the `loop_limit` from 1 to 20_000 only slightly increases `native` gas usage. For `wasmi interpreter`, however, it drives gas usage up to the limit per transaction of 300 TGas.
 
 # Log noise
 
--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,74 @@
-use workspaces::network::Sandbox;
+use clap::Parser;
+use comfy_table::Table;
+use rust_decimal::Decimal;
 use workspaces::types::Gas;
-use workspaces::Worker;
+use workspaces::Contract;
 
 /// The method to be called on contracts for benchmarking gas usage. It is expected to carry out the
 /// same calculations for every contract in `contracts`, either by executing them directly or by
 /// interpreting wasm.
 const METHOD_NAME: &str = "cpu_ram_soak";
 
-/// The number of iterations to execute in `contracts/calculations`.
-const LOOP_LIMIT: u32 = 100;
+/// Benchmark gas usage of interpreting wasm.
+///
+/// A smart contract doing calculations is executed on Near and interpreted inside another Near
+/// contract to compare the resulting gas usage.
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Args {
+    /// The number of times to loop calculations. Separate multiple values by ',' (no whitespace).
+    #[arg(long, num_args=1.., value_delimiter=',')]
+    loop_limit: Vec<u32>,
+}
 
 #[tokio::main]
-async fn main() {
-    let worker = workspaces::sandbox().await.expect("should spin up sandbox");
+async fn main() -> anyhow::Result<()> {
+    let cli_args = Args::parse();
 
-    let project_path_native = "./contracts/calculations";
-    let wasm_calculations = workspaces::compile_project(project_path_native)
-        .await
-        .expect("should compile contracts/calculations");
-    let gas_burnt_native = profile_gas_usage(
-        &worker,
-        &wasm_calculations,
-        LOOP_LIMIT.to_le_bytes().to_vec(),
-    )
-    .await
-    .expect("should profile gas usage (native calculations");
-    print_gas_burnt(project_path_native, gas_burnt_native);
+    let worker = workspaces::sandbox().await?;
 
-    let project_path_wasmi = "./contracts/calculations-in-wasmi";
-    let wasm_wasmi = workspaces::compile_project(project_path_wasmi)
-        .await
-        .expect("should compile contracts/calculations-calculations-in-wasmi");
-    // Passing `wasm_calculations` to interpret it in `wasm_wasi`.
-    let args: Vec<u8> = [LOOP_LIMIT.to_le_bytes().to_vec(), wasm_calculations].concat();
-    let gas_burnt_wasmi = profile_gas_usage(&worker, &wasm_wasmi, args)
-        .await
-        .expect("should profile gas usage (calculations in wasmi)");
-    print_gas_burnt(project_path_wasmi, gas_burnt_wasmi);
+    let wasm_calculations = workspaces::compile_project("./contracts/calculations").await?;
+    let contract_calculations = worker.dev_deploy(&wasm_calculations).await?;
+
+    let wasm_wasmi = workspaces::compile_project("./contracts/calculations-in-wasmi").await?;
+    let contract_wasmi = worker.dev_deploy(&wasm_wasmi).await?;
+
+    let mut gas_usage = GasUsage::default();
+
+    for loop_limit in cli_args.loop_limit {
+        gas_usage.loop_limits.push(loop_limit);
+
+        let gas_burnt_native = profile_gas_usage(
+            &contract_calculations,
+            loop_limit.to_le_bytes().to_vec(),
+            loop_limit,
+        )
+        .await?;
+        gas_usage.native.push(gas_burnt_native);
+
+        // Passing `wasm_calculations` to interpret it in `wasm_wasi`.
+        let args: Vec<u8> = [loop_limit.to_le_bytes().to_vec(), wasm_calculations.clone()].concat();
+        let gas_burnt_wasmi = profile_gas_usage(&contract_wasmi, args, loop_limit).await?;
+        gas_usage.interpreted_wasmi.push(gas_burnt_wasmi);
+    }
+
+    println!("{gas_usage}");
+
+    Ok(())
 }
 
 /// Returns the `Gas` burnt by the receipt corresponding to the `FunctionCallAction` of calling
-/// [`METHOD_NAME`] on contract `project_path` deployed to `worker`.
+/// [`METHOD_NAME`] on contract `project_path` deployed to `worker`. The logs of `contract_wasm` are
+/// used to verify that calculations were repeated `expected_loop_limit` times.
 ///
 /// Gas burnt by receipts for actions other than `FunctionCallAction` is not considered. They are
 /// due to overhead unrelated to the calculations to be benchmarked, like transaction to receipt
 /// conversion and gas refunds.
 async fn profile_gas_usage(
-    worker: &Worker<Sandbox>,
-    contract_wasm: &[u8],
+    contract: &Contract,
     method_args: Vec<u8>,
+    expected_loop_limit: u32,
 ) -> anyhow::Result<Gas> {
-    let contract = worker.dev_deploy(&contract_wasm).await?;
-
     let result = contract
         .call(METHOD_NAME)
         .args(method_args)
@@ -71,7 +88,7 @@ async fn profile_gas_usage(
     // executed in interpreted wasm. When interpreting wasm, the contract embedding the interpreter
     // is expected to forward guest logs to Near's `log_utf8`.
     assert_eq!(
-        vec![format!("Done {LOOP_LIMIT} iterations!")],
+        vec![format!("Done {expected_loop_limit} iterations!")],
         result.logs()
     );
 
@@ -87,8 +104,42 @@ async fn profile_gas_usage(
     Ok(gas_burnt)
 }
 
-fn print_gas_burnt(project_path: &str, gas_burnt: Gas) {
-    println!(
-        "Gas used by the `FunctionCallAction` receipt calling\n `{METHOD_NAME}` on {project_path}:\n {gas_burnt}"
-    );
+#[derive(Default)]
+/// Collects gas usage values for different `loop_limit` values.
+struct GasUsage {
+    /// Values of `loop_limit` for which benchmarks are run.
+    loop_limits: Vec<u32>,
+    /// Gas used by executing `contracts/calculations` directly for each of the `loop_limits`.
+    native: Vec<Gas>,
+    /// Gas used by executing a contract that uses wasmi to interpret `contracts/calculations` for
+    /// each of the `loop_limits`.
+    interpreted_wasmi: Vec<Gas>,
+}
+
+/// Prints gas usage values in a table.
+impl std::fmt::Display for GasUsage {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut headers = vec!["exec_mode".to_string()];
+        let mut row_native = vec!["native".to_string()];
+        let mut row_wasmi = vec!["wasmi interpreter".to_string()];
+        for i in 0..self.loop_limits.len() {
+            headers.push(format!("loop_limit = {}", self.loop_limits[i]));
+            row_native.push(fmt_tgas(self.native[i]));
+            row_wasmi.push(fmt_tgas(self.interpreted_wasmi[i]));
+        }
+
+        let mut table = Table::new();
+        table.set_header(headers);
+        table.add_row(row_native);
+        table.add_row(row_wasmi);
+
+        write!(f, "{}", table)
+    }
+}
+
+/// Formats `gas` as teragas, rounding to four decimals.
+fn fmt_tgas(gas: Gas) -> String {
+    // Scale 12 since converting gas to tgas requires dividing by 10^12.
+    let tgas = Decimal::new(gas.try_into().unwrap(), 12);
+    format!("{} TGas", tgas.round_dp(4))
 }
